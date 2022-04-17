d15380a2cb5ae4857908d3429ff8aee28b7bcd7e
IlyaEmeliyanov <ilya.emeliyanov20@gmail.com>
2022-04-17 16:06:09 +0200

    Fix routing problems

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { Fragment } from 'react'
 import { Routes, Route } from 'react-router-dom'
 
 // Importing components
-import Header from './components/common/Header'
 import Home from './components/Home'
 import TermsConditions from './components/TermsConditions'
 import PrivacyPolicy from './components/PrivacyPolicy'
@@ -16,8 +15,8 @@ function App() {
       <Fragment>
         <Routes>
           <Route path="/" element={<Home />}></Route>
-          <Route path="terms-conditions" element={<TermsConditions />}></Route>
-          <Route path="privacy-policy" element={<PrivacyPolicy />}></Route>
+          <Route path="/terms-conditions" element={<TermsConditions />}></Route>
+          <Route path="/privacy-policy" element={<PrivacyPolicy />}></Route>
         </Routes>
       </Fragment>
   );
